c5d2eb9817c8fdf8470d501e0313395414f027e6
Jared Atkinson <jared@invoke-ir.com>
2017-08-11 07:26:34 +0200

    Updated start.sh to provide access details
    start.sh provides the following access details upon completion:
    - Server Uri
    - ACE Admin User's ApiKey
    - SSL Thumbprint

--- ACE-Docker/start.sh
+++ ACE-Docker/start.sh
@@ -14,5 +14,30 @@ cat ace-app/appsettings.Template.json | sed 's/\[SSL THUMBPRINT\]/'"$fingerprint
 docker-compose build
 docker-compose up -d
 
-# Provide SSL Thumbprint for PowerShell Module
-echo "" && echo "Use the following Thumbprint when importing the ACE PowerShell module" && echo "   SSL Thumbprint: $fingerprint" && echo ""
+# Get IP Address
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     ip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}');;
+    Darwin*)    ip=$(ifconfig en0 | grep inet | grep -v inet6 | cut -d ' ' -f2);;
+    CYGWIN*)    ip=Cygwin;;
+    MINGW*)     ip=MinGw;;
+    *)          ip="UNKNOWN:${unameOut}"
+esac
+
+
+# Provide configuration details for PowerShell Module
+clear
+echo ""
+echo ""
+echo "=========================================================="
+echo "|      Thank you for provisioning ACE with Docker!!      |"
+echo "=========================================================="
+echo ""
+echo "Please use the following information to interact with ACE:" 
+echo "             Uri: https://$ip"
+echo "          ApiKey: d0bf91fa-9934-40ca-8cb9-5a1168546abc"
+echo "   SslThumbprint: $fingerprint"
+echo ""
+echo "=========================================================="
+echo ""
+echo ""
