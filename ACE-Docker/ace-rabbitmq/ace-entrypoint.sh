bfeef948411531ced4486d3fb471d0e69f78bbdf
Robby Winchester <robbywinchester@gmail.com>
2017-09-07 14:53:47 -0700

    Updated RabbitMQ

---
+++ ACE-Docker/ace-rabbitmq/ace-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python /root/ace-lookup.py -s 127.0.0.1 -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS -k $APIKEY &
+python /root/ace-cache.py -s 127.0.0.1 -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS &
+while true; do :; sleep 600; done
